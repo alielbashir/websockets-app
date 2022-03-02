2b6bc85224b2510f51b4ce83c1546d7f8fe341b2
Ali Elbashir <alielbashiir@gmail.com>
2022-03-02 23:18:00 +0000

    Add client side logging

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const socket = io('http://127.0.0.1:3000')
-
-
 // print data everytime a chat message event is actvated
 socket.on('chat-message', data => {
     console.log(data)
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const io = require('socket.io')(3000)
+import { Server } from "socket.io";
+
+const io = new Server(3003, {
+    cors: {
+        origin: "*"
+    }
+});
+
 
 io.on('connection', socket => {
+    console.log('New User')
     socket.emit('chat-message', 'Hiiii there')
 })
-
