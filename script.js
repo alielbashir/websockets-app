da758fc760082c5827d1aa35bf7e8ecf95ca0802
Ali Elbashir <alielbashiir@gmail.com>
2022-03-02 23:41:39 +0000

    Add user connection events

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 // print data everytime a chat message event is actvated
 
 const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+const messageContainer = document.getElementById('message-container')
+
+const name = prompt('What is your name?')
+appendMessage('You joined as ' + name)
+socket.emit('new-user', name)
+
 socket.on('chat-message', data => {
     console.log(data)
+    appendMessage(data)
+})
+
+socket.on('user-connected', data => {
+    console.log('user ' + data + ' has arrived!')
 })
 
 messageForm.addEventListener('submit', e => {
     // stop page from refreshing by default on submit event
     e.preventDefault()
+    const message = messageInput.value
+    socket.emit('send-chat-message', message)
+
+    // clear message after sending
+    messageInput.value = ''
 })
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import { Server } from "socket.io";
 
+const users = {}
+
 const io = new Server(3003, {
     cors: {
         origin: "*"
@@ -10,4 +12,13 @@ const io = new Server(3003, {
 io.on('connection', socket => {
     console.log('New User')
     socket.emit('chat-message', 'Hiiii there')
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message)
+    })
+
+    socket.on('new-user', name => {
+        console.log(name, 'connected')
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
 })
