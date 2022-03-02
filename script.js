3ed0b526a1b5c930d2b54f97bb2c0ce415b623ee
Ali Elbashir <alielbashiir@gmail.com>
2022-03-02 23:20:47 +0000

    Disable page refresh on send button click

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // print data everytime a chat message event is actvated
+
+const messageForm = document.getElementById('send-container')
 socket.on('chat-message', data => {
     console.log(data)
+})
+
+messageForm.addEventListener('submit', e => {
+    // stop page from refreshing by default on submit event
+    e.preventDefault()
 })
